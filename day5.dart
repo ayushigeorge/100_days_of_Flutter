d018adcee65f8a37ce812c39605550b3ece7ce8a
Ayushi George <ayushigeorge31@gmail.com>
2023-09-01 01:54:14 +0530

    Create day5.dart

---
+++ day5.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+      appBar: AppBar(
+          title:Text('My first Flutter project'),
+      ),
+    body: Center(
+      //property
+      child :   Text('Hello Enlighten Achievers!'),
+    ),
+  ),
+));
+
