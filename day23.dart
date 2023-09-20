ff128e31d1d7b42c726b18e9cd0ad1d9bf943046
Ayushi George <ayushigeorge31@gmail.com>
2023-09-20 22:48:12 +0530

    Create day23.dart
    with custome classes

---
+++ day23.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+void main() => runApp(MaterialApp(
+    home: QuoteList()
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  //  List<String> quotes = [
+  //    'Be yourself; everyone else is already taken',
+  //    'I have nothing to declare except my genius',
+  //    'The truth is rarely pure and never simple'
+  //  ];
+
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Robert Grene', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Carlo loss', text: 'The truth is rarely pure and never simple')
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+      ), //$ used to output the elements, we used {} to seal the property
+    );
+  }
+}
