ce476832d142a38d72b5c3a7747240e67687d985
Ayushi George <ayushigeorge31@gmail.com>
2023-09-20 22:49:30 +0530

    Update day23.dart

--- day23.dart
+++ day23.dart
@@ -39,3 +39,30 @@ class _QuoteListState extends State<QuoteList> {
     );
   }
 }
+
+// Custome CLsss
+class Quote {
+
+  String text;
+  String author;
+
+  //  normal constructor, as we've already seen
+
+  //  Quote(String author, String text){
+  //    this.text = text;
+  //    this.author = author;
+  //  }
+
+  //  constructor with named parameters
+
+  //  Quote({ String author, String text }){
+  //    this.text = text;
+  //    this.author = author;
+  //  }
+
+  // constructor with named parameters, doesnt matter of the order
+  // & automatically assigns named arguments to class properties
+
+  Quote({ this.text, this.author });
+
+}
